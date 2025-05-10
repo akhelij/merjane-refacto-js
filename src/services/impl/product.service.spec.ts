324acd99541cbc006676e31f7917f1d00e4e0940
mohamed akhelij <m.akhelij@gmail.com>
2025-05-10 15:43:17 +0100

    test: ensure test suite for SeasonalProduct and ExpiredProduct cases

--- src/services/impl/product.service.spec.ts
+++ src/services/impl/product.service.spec.ts
@@ -52,5 +52,187 @@ describe('ProductService Tests', () => {
 		});
 		expect(result).toEqual(product);
 	});
-});
 
+	describe('handleSeasonalProduct', () => {
+		const oneDayInMs = 1000 * 60 * 60 * 24;
+
+		it('should notify delay when product is in-season and lead time is within season', async () => {
+			// GIVEN
+			const currentDate = new Date();
+			const seasonStartDate = new Date(currentDate.getTime() - 10 * oneDayInMs);
+			const seasonEndDate = new Date(currentDate.getTime() + 30 * oneDayInMs);
+			const product: Product = {
+				id: 1,
+				leadTime: 5,
+				available: 0,
+				type: 'SEASONAL',
+				name: 'Product1',
+				expiryDate: null,
+				seasonStartDate,
+				seasonEndDate,
+			};
+			await databaseMock.insert(products).values(product);
+
+			// WHEN
+			await productService.handleSeasonalProduct(product);
+
+			// THEN
+			expect(notificationServiceMock.sendDelayNotification).toHaveBeenCalledWith(product.leadTime, product.name);
+			expect(notificationServiceMock.sendOutOfStockNotification).not.toHaveBeenCalled();
+			expect(notificationServiceMock.sendExpirationNotification).not.toHaveBeenCalled();
+			const result = await databaseMock.query.products.findFirst({
+				where: (p, {eq}) => eq(p.id, product.id),
+			});
+			expect(result).toEqual(product);
+		});
+
+		it('should send out of stock notification when product is in-season but lead time extends beyond season', async () => {
+			// GIVEN
+			const currentDate = new Date();
+			const seasonStartDate = new Date(currentDate.getTime() - 10 * oneDayInMs);
+			const seasonEndDate = new Date(currentDate.getTime() + 10 * oneDayInMs);
+			const product: Product = {
+				id: 1,
+				leadTime: 15,
+				available: 0,
+				type: 'SEASONAL',
+				name: 'Product1',
+				expiryDate: null,
+				seasonStartDate,
+				seasonEndDate,
+			};
+			await databaseMock.insert(products).values(product);
+
+			// WHEN
+			await productService.handleSeasonalProduct(product);
+
+			// THEN
+			expect(notificationServiceMock.sendOutOfStockNotification).toHaveBeenCalledWith(product.name);
+			expect(notificationServiceMock.sendDelayNotification).not.toHaveBeenCalled();
+			expect(notificationServiceMock.sendExpirationNotification).not.toHaveBeenCalled();
+			expect(product.available).toBe(0);
+			const result = await databaseMock.query.products.findFirst({
+				where: (p, {eq}) => eq(p.id, product.id),
+			});
+			expect(result).toEqual(product);
+		});
+
+		it('should send out of stock notification when current date is before season start', async () => {
+			// GIVEN
+			const currentDate = new Date();
+			const seasonStartDate = new Date(currentDate.getTime() + 15 * oneDayInMs);
+			const seasonEndDate = new Date(currentDate.getTime() + 45 * oneDayInMs);
+			const product: Product = {
+				id: 1,
+				leadTime: 5,
+				available: 0,
+				type: 'SEASONAL',
+				name: 'Product1',
+				expiryDate: null,
+				seasonStartDate,
+				seasonEndDate,
+			};
+			await databaseMock.insert(products).values(product);
+
+			// WHEN
+			await productService.handleSeasonalProduct(product);
+
+			// THEN
+			expect(notificationServiceMock.sendOutOfStockNotification).toHaveBeenCalledWith(product.name);
+			expect(notificationServiceMock.sendDelayNotification).not.toHaveBeenCalled();
+			expect(notificationServiceMock.sendExpirationNotification).not.toHaveBeenCalled();
+			const result = await databaseMock.query.products.findFirst({
+				where: (p, {eq}) => eq(p.id, product.id),
+			});
+			expect(result).toEqual(product);
+		});
+	});
+
+	describe('handleExpiredProduct', () => {
+		const oneDayInMs = 1000 * 60 * 60 * 24;
+
+		it('should decrement available when product is not expired and available > 0', async () => {
+			// GIVEN
+			const currentDate = new Date();
+			const expiryDate = new Date(currentDate.getTime() + 10 * oneDayInMs); // 10 days in future
+			const product: Product = {
+				id: 1,
+				leadTime: 5,
+				available: 10, // Available stock
+				type: 'EXPIRABLE',
+				name: 'Product1',
+				expiryDate,
+				seasonStartDate: null,
+				seasonEndDate: null,
+			};
+			await databaseMock.insert(products).values(product);
+
+			// WHEN
+			await productService.handleExpiredProduct(product);
+
+			// THEN
+			expect(product.available).toBe(9); // Should be decremented
+			expect(notificationServiceMock.sendExpirationNotification).not.toHaveBeenCalled();
+			const result = await databaseMock.query.products.findFirst({
+				where: (p, {eq}) => eq(p.id, product.id),
+			});
+			expect(result).toEqual(product);
+		});
+
+		it('should set available to 0 and send expiration notification when product is expired and available > 0', async () => {
+			// GIVEN
+			const currentDate = new Date();
+			const expiryDate = new Date(currentDate.getTime() - 5 * oneDayInMs); // 5 days ago (expired)
+			const product: Product = {
+				id: 1,
+				leadTime: 5,
+				available: 10, // Available stock but expired
+				type: 'EXPIRABLE',
+				name: 'Product1',
+				expiryDate,
+				seasonStartDate: null,
+				seasonEndDate: null,
+			};
+			await databaseMock.insert(products).values(product);
+
+			// WHEN
+			await productService.handleExpiredProduct(product);
+
+			// THEN
+			expect(product.available).toBe(0); // Should be set to 0
+			expect(notificationServiceMock.sendExpirationNotification).toHaveBeenCalledWith(product.name, product.expiryDate);
+			const result = await databaseMock.query.products.findFirst({
+				where: (p, {eq}) => eq(p.id, product.id),
+			});
+			expect(result).toEqual(product);
+		});
+
+		it('should set available to 0 and send expiration notification when product is not expired but available = 0', async () => {
+			// GIVEN
+			const currentDate = new Date();
+			const expiryDate = new Date(currentDate.getTime() + 10 * oneDayInMs); // 10 days in future
+			const product: Product = {
+				id: 1,
+				leadTime: 5,
+				available: 0, // No stock
+				type: 'EXPIRABLE',
+				name: 'Product1',
+				expiryDate,
+				seasonStartDate: null,
+				seasonEndDate: null,
+			};
+			await databaseMock.insert(products).values(product);
+
+			// WHEN
+			await productService.handleExpiredProduct(product);
+
+			// THEN
+			expect(product.available).toBe(0); // Should remain 0
+			expect(notificationServiceMock.sendExpirationNotification).toHaveBeenCalledWith(product.name, product.expiryDate);
+			const result = await databaseMock.query.products.findFirst({
+				where: (p, {eq}) => eq(p.id, product.id),
+			});
+			expect(result).toEqual(product);
+		});
+	});
+});
